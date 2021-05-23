0d7c1e2305e6f55a3da7e8ef47f3d2579e685f73
Alexander Kulumbeg <kulumbeg@gmail.com>
2021-05-23 15:43:05 +0200

    Translated yule.jl to Julia

---
+++ Julia/yule.jl
@@ -0,0 +1,23 @@
+using Chain, 
+      DataFrames, 
+      FileIO, 
+      GLM,
+      HTTP
+     
+
+yule = "https://raw.github.com/scunning1975/mixtape/master/yule.dta" |>
+    HTTP.download |>
+    load |>
+    DataFrame
+
+
+yule_lm = lm(@formula(paup ~ outrelief + old + pop), yule)
+
+# the summary() function workaround
+
+yule_lm                                      # print model            
+dof_residual(yule_lm)                        # degrees of freedom
+round(r2(yule_lm), digits = 4)               # r^2
+round(adjr2(yule_lm), digits = 4)            # r^2 adjusted
+null_model = lm(@formula(paup ~ 1), yule)    # create null null
+ftest(yule_lm.model, null_model.model)       # get the F value and significance
