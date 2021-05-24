fe103ec382285013dda62b8c5641a07841f1f993
Alexander Kulumbeg <kulumbeg@gmail.com>
2021-05-24 16:38:12 +0200

    Fixed a mistake in a comment

--- Julia/yule.jl
+++ Julia/yule.jl
@@ -19,5 +19,5 @@ yule_lm                                      # print model
 dof_residual(yule_lm)                        # degrees of freedom
 round(r2(yule_lm), digits = 4)               # r^2
 round(adjr2(yule_lm), digits = 4)            # r^2 adjusted
-null_model = lm(@formula(paup ~ 1), yule)    # create null null
+null_model = lm(@formula(paup ~ 1), yule)    # create null model
 ftest(yule_lm.model, null_model.model)       # get the F value and significance
