0bc68ace6849debc4a26828ba5f246cdaf1b7608
Daniel Winkler <danielw2904@disroot.org>
2021-05-28 19:51:02 +0200

    refactor

--- Julia/independence.jl
+++ Julia/independence.jl
@@ -2,18 +2,18 @@ using Chain,
       DataFrames,
       Random,
       Statistics
-
+Random.seed!(1)
 function gap()
     df = DataFrame(y1 = [7, 5, 5, 7, 4, 10, 1, 5, 3, 9], 
                    y0 = [1, 6, 1, 8, 2, 1, 10, 6, 7, 8],
                    random = randn(10))
 
     sd0 = @chain df begin
-        sort!([:random]) 
+        sort!(:random) 
         insertcols!(:d => vcat(fill(1,5), fill(0,5)))
-        insertcols!(:y => df.d .* df.y1 + (1 .- df.d) .* df.y0)
-        select!(:y)
-        convert(Array, _)
+        select!([:d, :y1, :y0] => 
+                ByRow((d,y1,y0) -> d * y1 + (1 - d) * y0) => :y)
+        _.y
     end
     
     sd0 = mean(sd0[1:5]-sd0[6:10])
