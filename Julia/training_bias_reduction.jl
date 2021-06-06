70cf653b9d491c3e794a588261207d85a20c1f62
Alexander Kulumbeg <kulumbeg@gmail.com>
2021-06-06 19:56:07 +0200

    Update training_bias_reduction.jl

--- Julia/training_bias_reduction.jl
+++ Julia/training_bias_reduction.jl
@@ -15,8 +15,7 @@ end
 
 training_bias_reduction = @chain read_data("training_bias_reduction.dta") begin
   insertcols!(_, :Y1 => ifelse.(_.Unit .<= 4, _.Y, missing))
-  insertcols!(_, :Y0 => [4,0,5,1,4,0,5,1])  
-  
+  insertcols!(_, :Y0 => [4,0,5,1,4,0,5,1])
 end 
 
 train_reg = lm(@formula(Y ~ X), training_bias_reduction)
